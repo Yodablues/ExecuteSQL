f23c0396a8b3d503f24456e51d229c75c4261898
Tom Colarusso <etcolarusso@gmail.com>
2014-03-22 02:53:01 -0400

    This is a Class file that takes a connection string name, stored procedure name and list collection of SqlParameters when instantiated. You than call Execute() which executes the stored procedure and returns a dataset.
    Example Usage:
    
    //Parameters are not required
    List<SqlParameter> sqlParameters = new List<SqlParameter>();
    sqlParameters.Add(new SqlParameter("@Id", 1));
    
    //string ConnectionStringName, string ProcedureName are required
    ExecuteStoredProcedure execute = new
    ExecuteStoredProcedure("NameOFConnectionString","ProcedureName"[,sqlParameters]);
    DataSet ds = execute.Execute();

---
+++ ExecuteStoreProcedure.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ExecuteSQL
+{
+    public class ExecuteStoredProcedure
+    {
+        private SqlConnection SqlConnection { get; set; }
+        private string ConnectionStringName { get; set; }
+        private string ProcedureName { get; set; }
+        private SqlCommand SqlCommand { get; set; }
+
+        public ExecuteStoredProcedure(string connectionStringName, string procedureName)
+        {
+            try
+            {
+                SqlConnection = SetupSQLConnection(connectionStringName);
+                ProcedureName = null;
+                SqlCommand = SetupSqlCommand(procedureName);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine
+                    ("Catching the {0} exception triggers the finally block.",
+                    ex.GetType());
+                throw;
+            }
+        }
+
+        public ExecuteStoredProcedure(string connectionStringName, string procedureName, List<SqlParameter> parameters)
+        {
+            try
+            {
+                SqlConnection = SetupSQLConnection(connectionStringName);
+                ProcedureName = procedureName;
+                SqlCommand = SetupSqlCommand(procedureName);
+                SetupSQLParameters(parameters);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine
+                    ("Catching the {0} exception triggers the finally block.",
+                    ex.GetType());
+                throw;
+            }
+        }
+
+        private SqlConnection SetupSQLConnection()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection();
+                return connection;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine
+                    ("Catching the {0} exception triggers the finally block.",
+                    ex.GetType());
+                throw;
+            }
+        }
+
+        private SqlConnection SetupSQLConnection(string connectionStringName)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection();
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+                return connection;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine
+                    ("Catching the {0} exception triggers the finally block.",
+                    ex.GetType());
+                throw;
+            }
+        }
+
+        private SqlCommand SetupSqlCommand()
+        {
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                SqlCommand.CommandType = CommandType.StoredProcedure;
+                SqlCommand.Connection = SqlConnection;
+                return sqlCommand;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine
+                    ("Catching the {0} exception triggers the finally block.",
+                    ex.GetType());
+                throw;                
+            }
+        }
+
+        public void SetupSQLParameters(List<SqlParameter> sqlParameters)
+        {
+            try
+            {
+                if (sqlParameters.Count > 0)
+                {
+                    foreach (SqlParameter param in sqlParameters)
+                    {
+                        SqlCommand.Parameters.Add(param);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine
+                    ("Catching the {0} exception triggers the finally block.",
+                    ex.GetType());
+                throw;
+            }
+        }
+
+        private SqlCommand SetupSqlCommand(string procedureName)
+        {
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = procedureName;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Connection = SqlConnection;
+                return sqlCommand;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine
+                    ("Catching the {0} exception triggers the finally block.",
+                    ex.GetType());
+                throw;                
+            }
+        }
+
+        public DataSet Execute()
+        {
+            try
+            {
+                DataSet dataSet = new DataSet();
+                SqlConnection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(SqlCommand);
+                dataAdapter.Fill(dataSet, "procedureResults");
+                SqlConnection.Close();
+                return dataSet;
+            }
+            catch(Exception ex)
+            {
+                SqlConnection.Close();
+                Console.WriteLine
+                    ("Catching the {0} exception triggers the finally block.",
+                    ex.GetType());
+                throw;  
+            }
+        }
+
+    }
+}
